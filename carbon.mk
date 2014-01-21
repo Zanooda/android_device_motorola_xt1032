6a209992a0bdd5439c2f57ee4ffe1e740430cf92
Daniel Storozhev <daniel.storozhev@gmail.com>
2014-01-21 16:20:06 +0100

    adjust files for carbon build

--- carbon.mk
+++ carbon.mk
@@ -1,5 +1,5 @@
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/carbon/config/common_phone.mk)
 
 # Boot animation
 TARGET_SCREEN_WIDTH := 720
@@ -7,7 +7,7 @@ TARGET_SCREEN_HEIGHT := 1280
 
 # Release name
 PRODUCT_RELEASE_NAME := MOTO G
-PRODUCT_NAME := cm_xt1032
+PRODUCT_NAME := carbon_xt1032
 
 $(call inherit-product, device/motorola/xt1032/full_xt1032.mk)
 
@@ -16,3 +16,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=XT1032 \
     BUILD_PRODUCT=falcon_umts \
     BUILD_FINGERPRINT=motorola/falcon_retuglb/falcon_umts:4.3/14.10.0Q3.X-84-16/6:user/release-keys
+
+PRODUCT_PROPERTY_OVERRIDES += ro.buildzipid=carbon.xt1032.$(shell date +%m%d%y).$(shell date +%H%M%S)
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,2 @@
-add_lunch_combo cm_xt1032-userdebug
+add_lunch_combo carbon_xt1032-userdebug
 
